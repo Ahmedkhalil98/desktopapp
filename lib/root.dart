82560a70745897d19be13885f5a143d3e1f85037
DilkhoshSaadon98 <fares.baqe@gmail.com>
2023-01-27 19:23:57 +0300

    add new

--- lib/root.dart
+++ lib/root.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           ),
           initialRoute: '/',
           getPages: [
-            // add by Dlo
+            // add by Dloss
             GetPage(name: "/", page: () => MyHomePage()),
             GetPage(name: "/profits", page: () => const ProfitsPage()),
             GetPage(name: "/import", page: () => EmportExportScreen()),
